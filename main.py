64de9c17456f4d154d896403e5798e4378c938d6
Ivan <zemzerovivan@gmail.com>
2025-03-14 20:10:34 +0300

    ыыы2

--- main.py
+++ main.py
@@ -12,5 +12,12 @@ def index(title):
     return render_template('index.html', **param)
 
 
+@app.route('/training/<prof>')
+def training(prof):
+    param = {}
+    param['prof'] = prof
+    return render_template('training.html', **param)
+
+
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8080)
