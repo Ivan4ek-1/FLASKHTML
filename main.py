60102e14374845072ba7c25d03468b0115a25f6d
Ivan <zemzerovivan@gmail.com>
2025-03-28 19:09:56 +0300

    listprof

--- main.py
+++ main.py
@@ -1,112 +1,72 @@
-import secrets
-from flask import Flask, render_template, redirect, request, make_response, session
-from data import db_session
-from data.users import User
-from data.news import News
-from forms.registerform import RegisterForm
-from flask_login import LoginManager, login_user, logout_user, current_user, login_required
-from forms.loginform import LoginForm
+from flask import *
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = secrets.token_hex(16)
-db_session.global_init("db/blogs.db")
-login_manager = LoginManager()
-login_manager.init_app(app)
+
+@app.route('/')
+def first():
+    return '<b>Миссия Колонизация Марса</b>'
 
 
-@app.route("/")
+@app.route('/index')
 def index():
-    db_sess = db_session.create_session()
-    if current_user.is_authenticated:
-        news = db_sess.query(News).filter(
-            (News.user == current_user) | (News.is_private != True))
-    else:
-        news = db_sess.query(News).filter(News.is_private != True)
-    return render_template("index.html", news=news)
+    return '<b>И на Марсе будут яблони цвести!</b>'
 
 
-@app.route('/register', methods=['GET', 'POST'])
-def reqister():
-    form = RegisterForm()
-    if form.validate_on_submit():
-        if form.password.data != form.password_again.data:
-            return render_template('register.html', title='Регистрация',
-                                   form=form,
-                                   message="Пароли не совпадают")
-        db_sess = db_session.create_session()
-        if db_sess.query(User).filter(User.email == form.email.data).first():
-            return render_template('register.html', title='Регистрация',
-                                   form=form,
-                                   message="Такой пользователь уже есть")
-        user = User(
-            name=form.name.data,
-            email=form.email.data,
-            about=form.about.data
-        )
-        user.set_password(form.password.data)
-        db_sess.add(user)
-        db_sess.commit()
-        return redirect('/login')
-    return render_template('register.html', title='Регистрация', form=form)
+@app.route('/promotion')
+def promotion():
+    return '''
+    <b>Человечество вырастает из детства.</b>
+    </br><b>Человечеству мала одна планета.</b>
+    </br><b>Мы сделаем обитаемыми безжизненные пока планеты.</b>
+    </br><b>И начнем с Марса!</b>
+    </br><b>Присоединяйся!</b>
+    '''
 
 
-@app.route("/cookie_test")
-def cookie_test():
-    visits_count = int(request.cookies.get("visits_count", 0))
-    if visits_count:
-        res = make_response(
-            f"Вы пришли на эту страницу {visits_count + 1} раз")
-        res.set_cookie("visits_count", str(visits_count + 1),
-                       max_age=60 * 60 * 24 * 365 * 2)
-    else:
-        res = make_response(
-            "Вы пришли на эту страницу в первый раз за последние 2 года")
-        res.set_cookie("visits_count", '1',
-                       max_age=60 * 60 * 24 * 365 * 2)
-    return res
+@app.route('/image_mars')
+def image_mars():
+    return f'''
+    <title>Привет, Марс!</title>
+    <h1><b>Жди нас, Марс!</b></h1>
+    <img src="{url_for('static', filename='images/MARS.png')}" alt='марс украли'>
+    </br>Вот она какая, красная планета.
+    '''
 
-
-@app.route("/session_test")
-def session_test():
-    visits_count = session.get('visits_count', 0)
-    session['visits_count'] = visits_count + 1
-    return make_response(
-        f"Вы пришли на эту страницу {visits_count + 1} раз")
-
-
-@login_manager.user_loader
-def load_user(user_id):
-    db_sess = db_session.create_session()
-    return db_sess.query(User).get(user_id)
-
-
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    form = LoginForm()
-    if form.validate_on_submit():
-        db_sess = db_session.create_session()
-        user = db_sess.query(User).filter(User.email == form.email.data).first()
-        if user and user.check_password(form.password.data):
-            login_user(user, remember=form.remember_me.data)
-            return redirect("/")
-        return render_template('login.html',
-                               message="Неправильный логин или пароль",
-                               form=form)
-    return render_template('login.html', title='Авторизация', form=form)
-
-
-@app.route('/logout')
-@login_required
-def logout():
-    logout_user()
-    return redirect("/")
-
-
-@app.route('/list_prof/<mod>')
-def list_prof(mod):
-    lst = ['инженер-исследователь', 'пилот', 'профессиональный комик']
-    return render_template('list.html', mod=mod, lst=lst)
+@app.route('/promotion_image')
+def promotion_image():
+    return f'''
+    <!doctype html>
+    <html lang="en">
+      <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
+         rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH"
+          crossorigin="anonymous">
+        <link rel="stylesheet" type="text/css" href="{url_for('static', filename='css/style.css')}" />
+        <title>Колонизация</title>
+      </head>
+      <body>
+        <h1><b>Жди нас, Марс!</b></h1>
+        <img src="{url_for('static', filename='images/MARS.jpg')}" alt='марс украли'>
+        <div class="alert alert-success" role="alert">
+          </br><b>Человечество вырастает из детства.</b>
+        </div>
+        <div class="alert alert-primary" role="alert">
+          </br><b>Человечеству мала одна планета.</b>
+        </div>
+        <div class="alert alert-danger" role="alert">
+          </br><b>Мы сделаем обитаемыми безжизненные пока планеты.</b>
+        </div>
+        <div class="alert alert-warning" role="alert">
+          </br><b>И начнем с Марса!</b>
+        </div>
+        <div class="alert alert-info" role="alert">
+          </br><b>Присоединяйся!</b>
+        </div>
+      </body>
+    </html>'''
 
 
 if __name__ == '__main__':
-    app.run(host="127.0.0.1", port=8080)
+    app.run(host='127.0.0.1', port=8000)
