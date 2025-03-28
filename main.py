7881934d0c6a96f845f7eccf50140f196b880f64
Ivan <zemzerovivan@gmail.com>
2025-03-28 20:09:45 +0300

    not ready  result otbora

--- main.py
+++ main.py
@@ -182,5 +182,38 @@ def choice(planet_name):
         </html>'''
 
 
+@app.route('/results/<nickname>/<int:level>/<float:rating>'):
+def results(nickname, level, rating):
+    return f'''
+            <!doctype html>
+            <html lang="en">
+              <head>
+                <meta charset="utf-8">
+                <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+                <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
+                 rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH"
+                  crossorigin="anonymous">
+                <link rel="stylesheet" type="text/css" href="{url_for('static', filename='css/style.css')}" />
+                <title>Результаты</title>
+              </head>
+              <body>
+                <h1>Результаты отбора</h1>
+                <h4>Претендента на участие в миссии {nickname}</h4>
+                <div class="alert alert-success" role="alert">
+                  <b>На ней много необходимых ресурсов;</b>
+                </div>
+                <div class="alert alert-primary" role="alert">
+                  <b>На ней есть вода и атмосфера;</b>
+                </div>
+                <div class="alert alert-danger" role="alert">
+                  <b>На ней есть небольшое магнитное поле;</b>
+                </div>
+                <div class="alert alert-warning" role="alert">
+                  ><b>Наконец, она просто красива!</b>
+                </div>
+              </body>
+            </html>'''
+
+
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8000)
