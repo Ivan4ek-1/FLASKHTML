e0687bd3a3b018a1d12536ffefa89aef78783dc5
Ivan <zemzerovivan@gmail.com>
2025-04-01 19:51:48 +0300

    fix

--- forms/loginform.py
+++ forms/loginform.py
@@ -6,7 +6,7 @@ from wtforms.validators import DataRequired
 class LoginForm(FlaskForm):
     username = StringField('id астронавта', validators=[DataRequired()])
     password = PasswordField('Пароль астронавта', validators=[DataRequired()])
-    captain_name = PasswordField('id капитана', validators=[DataRequired()])
+    captain_name = StringField('id капитана', validators=[DataRequired()])
     cap_password = PasswordField('Пароль капитана', validators=[DataRequired()])
     remember_me = BooleanField('Запомнить меня')
     submit = SubmitField('Доступ')
