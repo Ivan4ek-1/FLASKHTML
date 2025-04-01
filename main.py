ccf28bdfb0565c94ac824df5816a4352978c0589
Ivan <zemzerovivan@gmail.com>
2025-04-01 20:11:26 +0300

    cap

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 from flask import *
 
+from data import db_session
 from forms.loginform import LoginForm
+from data.users import User
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+
 
 @app.route('/')
 def first():
@@ -25,6 +28,7 @@ def list_prof(mod):
     lst = ['инженер-исследователь', 'пилот', 'профессиональный комик']
     return render_template('list.html', mod=mod, lst=lst)
 
+
 @app.route('/auto_answer')
 @app.route('/answer')
 def answer():
@@ -41,5 +45,50 @@ def login():
         return redirect('/success')
     return render_template('login.html', title='Аварийный доступ', form=form)
 
+
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=8000)
+    db_session.global_init("db/blogs.db")
+    #app.run(host='127.0.0.1', port=8000)
+
+
+captain = User()
+captain.surname = "Scott"
+captain.name = "Ridley"
+captain.age = 21
+captain.position = 'captain'
+captain.speciality = 'research engineer'
+captain.address = 'module_1'
+captain.email = "scott_chief@mars.org"
+
+chef = User()
+chef.surname = "Scott"
+chef.name = "Ridley"
+chef.age = 21
+chef.position = 'captain'
+chef.speciality = 'research engineer'
+chef.address = 'module_1'
+chef.email = "scott_chief@mars.org"
+
+captain = User()
+captain.surname = "Scott"
+captain.name = "Ridley"
+captain.age = 21
+captain.position = 'captain'
+captain.speciality = 'research engineer'
+captain.address = 'module_1'
+captain.email = "scott_chief@mars.org"
+
+captain = User()
+captain.surname = "Scott"
+captain.name = "Ridley"
+captain.age = 21
+captain.position = 'captain'
+captain.speciality = 'research engineer'
+captain.address = 'module_1'
+captain.email = "scott_chief@mars.org"
+
+
+
+db_sess = db_session.create_session()
+db_sess.add(captain)
+db_sess.commit()
